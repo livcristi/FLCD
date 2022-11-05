dcb58b7a28d22f7d0fc0a2befb79b429859167b7
Liviu-Terebes <72067145+livcristi@users.noreply.github.com>
2022-11-05 21:07:16 +0200

    Lab 3 (#3)
    * Added the required files
    
    * Added the documentation
    
    * Updated the scanner

--- Lab 3/main.py
+++ Lab 3/main.py
@@ -4,7 +4,6 @@ if __name__ == '__main__':
     scanner = Scanner()
 
     pif, id_table, const_table = scanner.scan_file('p1.pt')
-    print(pif)
-    print(id_table)
-    print(const_table)
-    print()
+    print("pif = ", pif)
+    print("id_table = ", id_table)
+    print("const_table = ", const_table)
