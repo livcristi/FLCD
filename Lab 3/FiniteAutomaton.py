b7cd6f9a786d9e093458e945528505bfa3698f8c
Liviu-Terebes <72067145+livcristi@users.noreply.github.com>
2022-11-14 19:59:51 +0200

    Lab 4 (#4)
    * Implemented the Finite Automata class
    
    * Added the FA in Lab3
    
    * Added the documentation

---
+++ Lab 3/FiniteAutomaton.py
@@ -0,0 +1,110 @@
+class FiniteAutomaton:
+    def __init__(self):
+        self.__states = set()
+        self.__alphabet = set()
+        self.__transitions = {}
+        self.__initial_state = None
+        self.__final_states = set()
+
+    def get_states(self):
+        return self.__states
+
+    def get_alphabet(self):
+        return self.__alphabet
+
+    def get_transitions(self):
+        return self.__transitions
+
+    def get_initial_state(self):
+        return self.__initial_state
+
+    def get_final_states(self):
+        return self.__final_states
+
+    def read_from_file(self, filename):
+        # Check if the file data is valid
+        if not self.__is_file_data_valid(filename):
+            return None
+        self.__clear_fields()
+        with open(filename) as inp_file:
+            # Read the valid states, alphabet, initial state and final states
+            self.__states = {state for state in inp_file.readline().strip().split()}
+            self.__alphabet = {symbol for symbol in inp_file.readline().strip().split()}
+            self.__initial_state = inp_file.readline().strip().split()[0]
+            self.__final_states = {element for element in inp_file.readline().strip().split()}
+
+            # Read the transition operations
+            for line in inp_file.readlines():
+                initial_state, term, final_state = line.strip().split()
+                if (initial_state, term) not in self.__transitions:
+                    self.__transitions[(initial_state, term)] = [final_state]
+                else:
+                    self.__transitions[(initial_state, term)].append(final_state)
+        return self
+
+    def is_dfa(self):
+        return self.__initial_state and all(len(result) <= 1 for result in self.__transitions.values())
+
+    def verify_sequence(self, sequence):
+        # Check if the FA is a DFA
+        if not self.is_dfa():
+            return None
+        current_state = self.__initial_state
+
+        for symbol in sequence:
+            # Check if there is a transition with the current state and symbol
+            if (current_state, symbol) not in self.__transitions.keys():
+                return False
+            current_state = self.__transitions[(current_state, symbol)][0]
+
+        if current_state in self.__final_states:
+            return True
+        else:
+            return False
+
+    def __clear_fields(self):
+        self.__states = set()
+        self.__alphabet = set()
+        self.__transitions = {}
+        self.__initial_state = None
+        self.__final_states = set()
+
+    @staticmethod
+    def __is_file_data_valid(filename):
+        with open(filename) as inp_file:
+            lines = inp_file.readlines()
+
+            # Retrieve the states
+            if len(lines) < 4:
+                # The file must have at least 4 rows (states, alphabet, initial, final state)
+                return False
+
+            valid_states = {state for state in lines[0].strip().split()}
+
+            # Retrieve the alphabet
+            alphabet = {symbol for symbol in lines[1].strip().split()}
+
+            # Retrieve the initial states
+            line = lines[2].strip().split()
+            if len(line) > 1 or line[0] not in valid_states:
+                # The line with the initial state must have one value only, which is a valid state
+                return False
+
+            # Retrieve the final states
+            line = lines[3].strip().split()
+            if any(state not in valid_states for state in line):
+                # All final states must be valid
+                return False
+
+            # Retrieve the transitions
+            for line in lines[4:]:
+                tokens = line.strip().split()
+                if len(tokens) != 3:
+                    # Each transition must have 3 parts (start state, token, next state)
+                    return False
+                if (tokens[0] not in valid_states) or \
+                        (tokens[1] not in alphabet) or \
+                        (tokens[2] not in valid_states):
+                    # Each part of the transition must be valid
+                    return False
+        return True
---
+++ Lab 4/UI.py
@@ -0,0 +1,75 @@
+class UI:
+    def __init__(self, FA):
+        self.__FA = FA
+
+    def __read_from_file(self):
+        filename = input('Enter the filename: ')
+        response = self.__FA.read_from_file(filename)
+        if response is not None:
+            print('The Finite Automaton was read from the file')
+        else:
+            print('The given file does not have data in a valid format. The FA was not read')
+
+    def __display_states(self):
+        print('The states are: ' + ' '.join(self.__FA.get_states()))
+
+    def __display_alphabet(self):
+        print('The alphabet is: ' + ' '.join(self.__FA.get_alphabet()))
+
+    def __display_transitions(self):
+        transitions = self.__FA.get_transitions()
+        print('The transitions are: ')
+        for state_transitions in transitions:
+            for result in transitions[state_transitions]:
+                print('({}, {}) -> {}'.format(state_transitions[0], state_transitions[1], result))
+
+    def __display_initial_state(self):
+        print(self.__FA.get_initial_state())
+
+    def __display_final_states(self):
+        print(' '.join(self.__FA.get_final_states()))
+
+    def __check_dfa(self):
+        if self.__FA.is_dfa():
+            print('The FA is DFA')
+        else:
+            print('The FA is not DFA')
+
+    def __verify_sequence(self):
+        user_sequence = input('Enter a sequence: ')
+        result = self.__FA.verify_sequence(user_sequence)
+        if result is None:
+            print('The FA is not a DFA. Cannot check sequence')
+        elif result:
+            print('The sequence is accepted by the DFA')
+        else:
+            print('The sequence is not accepted by the DFA')
+
+    @staticmethod
+    def __print_menu():
+        print('0. Close the menu')
+        print('1. Read FA from file')
+        print('2. Display the states of the FA')
+        print('3. Display the alphabet of the FA')
+        print('4. Display the transitions of the FA')
+        print('5. Display the initial state of the FA')
+        print('6. Display the final states of the FA')
+        print('7. Check if the FA is a DFA')
+        print('8. Verify a sequence')
+
+    def run(self):
+        options = {'1': self.__read_from_file, '2': self.__display_states, '3': self.__display_alphabet,
+                   '4': self.__display_transitions, '5': self.__display_initial_state, '6': self.__display_final_states,
+                   '7': self.__check_dfa, '8': self.__verify_sequence}
+
+        is_over = False
+
+        while not is_over:
+            self.__print_menu()
+            user_input = input('Enter your option: ').strip()
+            if user_input == '0':
+                is_over = True
+            elif user_input in options:
+                options[user_input]()
+            else:
+                print('Incorrect input! Please try again')
